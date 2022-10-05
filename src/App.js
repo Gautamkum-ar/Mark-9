4ff7c169b58770ebf897bfcb9dcb186bf4acfcdf
Gautam Kumar <gkvc9696@gmail.com>
2022-10-05 15:59:55 +0530

    Rebased with CREATE-REACT-APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./Data";
+import { useState } from "react";
+export default function App() {
+  const [store, setStore] = useState(data);
+  const [selectedCategory, setSelectedCategory] = useState("Beer");
+  const category = [...new Set(data.map((item) => item.category))];
 
-function App() {
+  const handleSelectCategory = (categoryName) => {
+    setSelectedCategory(categoryName);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head">Liquor Recommendation App</div>
+      <div className="categories">
+        {category.map((eachCategory, index) => {
+          const isSelected = eachCategory === selectedCategory;
+          return (
+            <button
+              disabled={isSelected}
+              key={index}
+              onClick={() => handleSelectCategory(eachCategory)}
+            >
+              {eachCategory}
+            </button>
+          );
+        })}
+      </div>
+      <div className="items">
+        {store
+          .filter((eachCategory) => eachCategory.category === selectedCategory)
+          .map((eachItem, index) => (
+            <div key={index}>
+              <h3>Name: {eachItem.name} </h3>
+              <h4>Category: {eachItem.category} </h4>
+              <p> {eachItem.rating} </p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Data.js
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    name: "Budweiser Magnum",
+    category: "Beer",
+    rating: 4.5,
+  },
+  {
+    name: "Kingfisher Premium",
+    category: "Beer",
+    rating: 4.2,
+  },
+  {
+    name: "Tuborg Gold",
+    category: "Beer",
+    rating: 4.2,
+  },
+  {
+    name: "Grover Zampa La Reserva",
+    category: "Wine",
+    rating: 4.5,
+  },
+  {
+    name: "Sette 2011",
+    category: "Wine",
+    rating: 4.5,
+  },
+  {
+    name: "Chateau De La Tour",
+    category: "Wine",
+    rating: 4.5,
+  },
+  {
+    name: "Magic Moments",
+    category: "Vodka",
+    rating: 4.2,
+  },
+  {
+    name: "Smirnoff",
+    category: "Vodka",
+    rating: 4.2,
+  },
+  {
+    name: "Absolut",
+    category: "Vodka",
+    rating: 4.2,
+  },
+];
